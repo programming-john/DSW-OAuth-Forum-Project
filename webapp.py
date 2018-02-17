e8268e85b6b88f9089352b9e1e763d7c2182ef24
franny1615 <31251485+franny1615@users.noreply.github.com>
2018-02-17 08:16:55 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -43,7 +43,9 @@ def post():
     #This function should add the new post to the JSON file of posts and then render home.html and display the posts.  
     #Every post should include the username of the poster and text of the post.
     try:
-        mess = "I got inside the try"
+        with open(file,'r+') as f:
+            data = json.load(f)
+            data.append(request.form['message'])
     except:
         mess = "There are no posts as of yet"
     return render_template('home.html', past_posts=posts_to_html(),rar=mess)
